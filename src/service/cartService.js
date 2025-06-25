a8ebe207b5fae38feafbb0c0c9f113989ad8ea49
janisar392 <janisarakhtar392@gmail.com>
2025-06-26 00:11:04 +0530

    update

--- src/service/cartService.js
+++ src/service/cartService.js
@@ -1,37 +1,39 @@
 import axios from "axios";
 
-const API_URL ='http://localhost:8080/api/cart';
+const API_URL = 'https://tastytrek.onrender.com/api/cart';
 
-export const addToCart = async (foodId , token) => {
+export const addToCart = async (foodId, token) => {
     try {
-         await axios.post(
-            'http://localhost:8080/api/cart',
-             {foodId}, 
-             {headers:{Authorization : `Bearer ${token}`}});
+        await axios.post(
+            API_URL,
+            { foodId },
+            { headers: { Authorization: `Bearer ${token}` } }
+        );
     } catch (error) {
-        console.error('Error while adding the cart data',error);
+        console.error('Error while adding to the cart', error);
     }
-}
+};
 
-export const removeQtyFromCart = async (foodId , token) => {
+export const removeQtyFromCart = async (foodId, token) => {
     try {
-         await axios.post(
-            'http://localhost:8080/api/cart/remove',
-             {foodId}, 
-             {headers:{Authorization : `Bearer ${token}`}});
+        await axios.post(
+            `${API_URL}/remove`,
+            { foodId },
+            { headers: { Authorization: `Bearer ${token}` } }
+        );
     } catch (error) {
-        console.error('Error while remove qty from the cart data',error);
+        console.error('Error while removing quantity from the cart', error);
     }
-}
+};
 
 export const getCartData = async (token) => {
     try {
-        const response = await axios.get("http://localhost:8080/api/cart",{
-            headers:{Authorization:`Bearer ${token}`}
-         });
-         return response.data.items || [];
+        const response = await axios.get(API_URL, {
+            headers: { Authorization: `Bearer ${token}` }
+        });
+        return response.data.items || [];
     } catch (error) {
-        console.error('Error while fetching the cart data',error);
+        console.error('Error while fetching the cart data', error);
         return [];
     }
-}
+};
